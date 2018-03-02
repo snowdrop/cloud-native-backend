991d87e9c64bccfd443d3ee08b965606137c87c9
Charles Moulliard <cmoulliard@redhat.com>
2018-03-02 16:48:49 +0100

    Remove Await Annotation as health is not defined

--- src/test/java/me/snowdrop/cloudnative/backend/OpenShiftIT.java
+++ src/test/java/me/snowdrop/cloudnative/backend/OpenShiftIT.java
@@ -3,7 +3,6 @@ package me.snowdrop.cloudnative.backend;
 import java.net.URL;
 
 import com.jayway.restassured.RestAssured;
-import org.arquillian.cube.openshift.impl.enricher.AwaitRoute;
 import org.arquillian.cube.openshift.impl.enricher.RouteURL;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Before;
@@ -12,9 +11,8 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class OpenShiftIT extends AbstractBoosterApplicationTest {
 
-    @AwaitRoute(path = "/health")
     @RouteURL("${app.name}")
-    private URL baseURL;
+    public URL baseURL;
 
     @Before
     public void setup() throws Exception {
