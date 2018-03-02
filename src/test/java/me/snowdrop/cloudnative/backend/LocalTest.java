13d5001c38928bd5bfb278ddb8654a9479b74b7e
Georgios Andrianakis <geoand@gmail.com>
2018-03-02 18:46:03 +0200

    Fix tests
    The integration test cannot use any Spring infrastructure and can only
    interact with the app via HTTP.
    Furthermore the actuator was added in order to give the application a
    health endpoint which is needed by Arquillian in order to ensure that
    the application is functioning before launching the test

--- src/test/java/me/snowdrop/cloudnative/backend/LocalTest.java
+++ src/test/java/me/snowdrop/cloudnative/backend/LocalTest.java
@@ -1,25 +1,57 @@
 package me.snowdrop.cloudnative.backend;
 
 import com.jayway.restassured.RestAssured;
+import me.snowdrop.cloudnative.backend.model.Note;
 import me.snowdrop.cloudnative.backend.repository.NoteRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.core.Is.is;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class LocalTest extends AbstractBoosterApplicationTest {
+public class LocalTest {
 
     @Value("${local.server.port}")
     private int port;
+
+    @Autowired
+    protected NoteRepository noteRepository;
 
     @Before
     public void beforeTest() {
         noteRepository.deleteAll();
         RestAssured.baseURI = String.format("http://localhost:%d/api/notes", port);
+    }
+
+    @Test
+    public void testGetEmptyArray() {
+        when().get()
+                .then()
+                .statusCode(200)
+                .body(is("[]"));
+    }
+
+
+    @Test
+    public void testGetOneNote() {
+        Note cherry = new Note();
+        cherry.setContent("cherry");
+        cherry.setTitle("excellent");
+        noteRepository.save(cherry);
+
+        when().get(String.valueOf(cherry.getId()))
+                .then()
+                .statusCode(200)
+                .body("id", is(cherry.getId().intValue()))
+                .body("content", is(cherry.getContent()))
+                .body("title", is(cherry.getTitle()));
     }
 
 }
